e7ee2d6a0041d5244e3c0831c61323add8b05834
FabioZ02 <zulianifabio5@gmail.com>
2026-04-14 15:37:57 +0200

    Possible Output class

--- BT_Data.cc
+++ BT_Data.cc
@@ -88,3 +88,92 @@ BT_Input::BT_Input(const std::string& file_name)
         for(unsigned id : orders[i].valid_resource_ids)
             compatibility_matrix[i][id - 1] = true;
 }
+
+BT_Output::BT_Output(const BT_Input& my_in)
+  : in(my_in), assigned_resource(in.OrdersCount(), -1), assigned_period(in.OrdersCount(), -1), load(in.ResourcesCount())
+{}
+
+BT_Output& BT_Output::operator=(const BT_Output& out)
+{
+    assigned_resource = out.assigned_resource;
+    assigned_period   = out.assigned_period;
+    load              = out.load;
+    return *this;
+}
+
+void BT_Output::Assign(unsigned t, unsigned r, unsigned p)
+{
+    // Task t was already assigned
+    if(IsAssigned(t))
+    {
+        unsigned old_r = assigned_resource[t];
+        unsigned old_p = assigned_period[t];
+
+        load[old_r][old_p] -= in.Order_Quantity(t);
+    }
+
+    // Otherwise
+    assigned_resource[t] = r;
+    assigned_period[t]   = p;
+
+    load[r][p] += in.Order_Quantity(t);
+
+}
+
+void BT_Output::Reset()
+{
+    unsigned t, r, p;
+
+    for(t =0; t < assigned_resource.size(); t++)
+    {
+        assigned_resource[t] = -1;
+    }
+
+     for(t =0; t < assigned_period.size(); t++)
+    {
+        assigned_period[t] = -1;
+    }
+
+    for(r = 0; r < in.ResourcesCount(); r++)
+    {
+        for(p = 0; p < load[r].size(); p++)
+        {
+            load[r][p] = 0;
+        }   
+    }
+}
+
+void BT_Output::Dump(std::ostream& os) const
+{
+    os << "=== Task Assignment ===" << std::endl;
+    for (unsigned t = 0; t < assigned_resource.size(); t++)
+    {
+        os << "Task " << t << " -> ";
+        if (IsAssigned(t)) {
+            os << "Machine " << assigned_resource[t] 
+               << ", Period " << assigned_period[t] << std::endl;
+        } else {
+            os << "Not Assigned" << std::endl;
+        }
+    }
+
+    os << "\n=== Load ===" << std::endl;
+    for (unsigned r = 0; r < load.size(); r++)
+    {
+        for (unsigned p = 0; p < load[r].size(); p++)
+        {
+            if (load[r][p] > 0)
+            {
+                os << "Period " << p << " - Machine " << r 
+                   << " : Load = " << load[r][p] << std::endl;
+            }
+        }
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const BT_Output& out)
+{
+    out.Dump(os);
+    return os;
+}
+
