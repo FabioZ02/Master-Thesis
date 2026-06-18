a3a072c4be73839daa62b40650e59ce42766d0cf
FabioZ02 <zulianifabio5@gmail.com>
2026-06-18 11:47:15 +0200

    Adapting the BT_Main.cc into the Main with the Union Neighborhood

--- BT_Main.cc
+++ BT_Main.cc
@@ -12,6 +12,12 @@ int main(int argc, const char* argv[])
     Parameter<string> instance("instance", "Input instance", main_parameters);
     Parameter<int> seed("seed", "Random seed", main_parameters);
     Parameter<string> init_state("init_state", "Initial state (to be read from file)", main_parameters);
+    Parameter<string> method("method", "Solution method (empty for tester)", main_parameters);
+    Parameter<string> output_file("output_file", "Write the output to a file (filename required)", main_parameters);
+    Parameter<double> swap_rate("swap_rate", "Swap rate", main_parameters); // for the union neighborhood
+    Parameter<bool> irace("irace", "IRace mode", main_parameters); // For the tuning with irace
+    swap_rate = 0.5;
+    irace = false;
 
     CommandLineParameters::Parse(argc, argv, false, true);
 
@@ -21,6 +27,7 @@ int main(int argc, const char* argv[])
         return 1;
     }
     BT_Input in(instance);
+    BT_Output out(in);
 
     if(seed.IsSet())
         Random::SetSeed(seed);
@@ -69,19 +76,66 @@ int main(int argc, const char* argv[])
     BT_swap_nhe.AddDeltaCostComponent(dcc4_swap);
     BT_swap_nhe.AddDeltaCostComponent(dcc5_swap);
 
+    // Union Neighbourhood
+    SetUnionNeighborhoodExplorer<BT_Input, BT_Output, DefaultCostStructure<int>, decltype(BT_shift_nhe), decltype(BT_swap_nhe)> bt_bnhe(in, BT_sm, "ShiftOrSwap", BT_shift_nhe, BT_swap_nhe, {1 - swap_rate, swap_rate});
+
+    // Simulated Annealing using union neighborhood
+    SimulatedAnnealing<BT_Input, BT_Output, decltype(bt_bnhe)::MoveType> bt_bsa(in, BT_sm, bt_bnhe, "BSA");
+    SimulatedAnnealingWithReheating<BT_Input, BT_Output, decltype(bt_bnhe)::MoveType> bt_bsawr(in, BT_sm, bt_bnhe, "BSAwr");
+
     // Tester
     Tester<BT_Input, BT_Output> tester(in, BT_sm);
     MoveTester<BT_Input, BT_Output, BT_Shift> shift_move_test(in, BT_sm, BT_shift_nhe, "BT_Shift move", tester);
     MoveTester<BT_Input, BT_Output, BT_Swap>  swap_move_test(in, BT_sm, BT_swap_nhe,  "BT_Swap move",  tester);
 
+    //Tester for the union neighborhood
+    MoveTester<BT_Input, BT_Output, decltype(bt_bnhe)::MoveType> bimodal_test(in, BT_sm, bt_bnhe, "ShiftOrSwap move", tester);
+
+    SimpleLocalSearch<BT_Input, BT_Output> bt_solver(in, BT_sm, "BT SOlver");
+
     if(!CommandLineParameters::Parse(argc, argv, true, false))
         return 1;
-
-    // Enter tester
-    if(init_state.IsSet())
-        tester.RunMainMenu(init_state);
+ 
+    if (!method.IsSet())
+        {
+            if(init_state.IsSet())
+            tester.RunMainMenu(init_state);
+            else
+            tester.RunMainMenu();
+        }
     else
-        tester.RunMainMenu();
-
+        {
+            if (method == string("BSA"))
+              bt_solver.SetRunner(bt_bsa);
+            else if (method == string("BSAwr"))
+              bt_solver.SetRunner(bt_bsawr);
+            else
+                {
+                    cerr << "Error: method " << string(method) << " not recognized" << endl;
+                    exit(1);
+                }
+            SolverResult<BT_Input, BT_Output> result = bt_solver.Solve();
+            out = result.output;
+            if (irace)
+               cout << result.cost.total << endl;
+            else if (output_file.IsSet())
+               {
+                  ofstream os(output_file);
+                  os << out << endl;
+                  os << "Cost: " << result.cost.total << endl;
+                  os << "Time: " << result.running_time << endl;
+                  os.close();
+                  string instance_name = instance;
+                  instance_name = instance_name.substr(instance_name.find_last_of("/") + 1);
+                  instance_name = instance_name.substr(0, instance_name.find_last_of("."));
+                  cout << instance_name << ": " << result.cost.total << " (time: " << result.running_time << ")" << endl;
+               }
+            else
+               {
+                   cout << out << endl;
+                   cout << "Cost: " << result.cost.total << endl;
+                   cout << "Time: " << result.running_time << endl;
+               }
+         }
     return 0;
 }
